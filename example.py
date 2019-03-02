d5ea4f12a4de663c854fb84afe822cacdf9b9a07
amazed <sfa>
2019-03-02 15:38:58 +0800

    yangjiao

--- example.py
+++ example.py
@@ -6,6 +6,74 @@ print(sess.run(hello))
 a = tf.constant(10)
 b = tf.constant(32)
 print(sess.run(a+b))
-print(1+1+1)
-print(23+22)
-print(2+4)
+
+import tensorflow as tf
+
+# 创建一个常量 op, 产生一个 1x2 矩阵. 这个 op 被作为一个节点
+# 加到默认图中.
+#
+# 构造器的返回值代表该常量 op 的返回值.
+matrix1 = tf.constant([[3., 3.]])
+
+# 创建另外一个常量 op, 产生一个 2x1 矩阵.
+matrix2 = tf.constant([[2.],[2.]])
+
+# 创建一个矩阵乘法 matmul op , 把 'matrix1' 和 'matrix2' 作为输入.
+# 返回值 'product' 代表矩阵乘法的结果.
+product = tf.matmul(matrix1, matrix2)
+
+
+sess = tf.Session()
+result = sess.run(product)
+print(result)
+sess.close()
+
+import tensorflow as tf
+sess = tf.InteractiveSession()
+x = tf.Variable([1.0,2.0])
+a = tf.constant([3.0,3.0])
+x.initializer.run()
+sub = tf.subtract(x,a)
+print(sub.eval())
+
+# 创建一个变量, 初始化为标量 0.
+state = tf.Variable(0, name="counter")
+
+# 创建一个 op, 其作用是使 state 增加 1
+
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+# 启动图后, 变量必须先经过`初始化` (init) op 初始化,
+# 首先必须增加一个`初始化` op 到图中.
+init_op = tf.global_variables_initializer()
+
+# 启动图, 运行 op
+with tf.Session() as sess:
+  # 运行 'init' op
+  sess.run(init_op)
+  # 打印 'state' 的初始值
+  print(sess.run(state))
+  # 运行 op, 更新 'state', 并打印 'state'
+  for _ in range(3):
+    sess.run(update)
+    print(sess.run(state))
+
+input1 = tf.constant(3.0)
+input2 = tf.constant(2.0)
+input3 = tf.constant(5.0)
+intermed = tf.add(input2, input3)
+mul = tf.multiply(input1, intermed)
+
+with tf.Session() as sess:
+  result = sess.run([mul, intermed])
+  print(result)
+
+
+input1 = tf.placeholder(tf.float32)
+input2 = tf.placeholder(tf.float32)
+output = tf.multiply(input1, input2)
+
+with tf.Session() as sess:
+  print(sess.run([output], feed_dict={input1:[7.], input2:[2.]}))
